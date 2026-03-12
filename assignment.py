8fd4b626d6ac865b6f31b573702a132977b1bafe
samuel <sammuthama968@gmail.com>
2026-03-12 15:07:39 +0300

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,1 +1,21 @@
+# Question 4 Write a python program that prints the following message "Hello AI studemts"
+print("Hello AI students")
+
+
+# Question 5 Create the following variables in Python:
+name
+age
+city
+Print them so the output looks like this:
+Name: John
+Age: 20
+City: Nairobi
+
+ name = "john"
+ age = 20
+ city = "Nairobi"
+print("Name:", name)
+print("Age:", age)
+print("city:", city)
+
 
